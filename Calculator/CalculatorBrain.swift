6febf2fa2bcc624506f827039a8750dd2f681377
davidyoon <qqpo12@naver.com>
2021-07-11 22:43:57 +0900

    delete useless code and add comments

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -19,6 +19,7 @@ class CalculatorBrain {
         accumulator = operand
     }
     
+    // 딕셔너리 value 값에는 상수, 함수, 2항 함수들이 들와야하는데 Double로 받으면 문제가 있으니 Enum으로 해당 값들을 모두 받게 구현
     private var operations: Dictionary<String,Operation> = [
         "π" : Operation.Constant(Double.pi), //Double.pi,
         "e" : Operation.Constant(M_E), //M_E,
@@ -32,6 +33,7 @@ class CalculatorBrain {
         "=" : Operation.Equals,
     ]
     
+    // 상수는 더블 일항연산자, 이항 연산자는 함수
     private enum Operation {
         case Constant(Double)
         case UnaryOperation((Double) -> Double )
@@ -48,12 +50,11 @@ class CalculatorBrain {
             case .UnaryOperation(let function):
                 accumulator = function(accumulator)
             case .BinaryOperation(let function):
-                executePendingBinaryOperation()
+                executePendingBinaryOperation() // 이항 연산자가 또 들어왔을 때 Equals와 같은 효과 나타나도록 추가 
+                // 3 x 5 =  에서 3 은 firstOperand, binaryFunciton은 연산자에 따라 4칙 연산 함수  다음 상수 5 들어고  eqauls 연산자 들어오면 해당 실행
                 pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
             case .Equals:
                 executePendingBinaryOperation()
-            default:
-                break
             }
         }
         
@@ -62,7 +63,8 @@ class CalculatorBrain {
     
     private func executePendingBinaryOperation() {
         if pending != nil {
-            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            print("실행이 되나 ")
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator) // 연산함수( FO, 방금 들어온 값 )
             pending = nil
         }
     }
